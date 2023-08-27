de1170924a35f89606ec84df02101697396c958a
Peter Pham <Peterpham68@gmail.com>
2023-08-27 00:23:50 -0400

    a

--- client/src/App.js
+++ client/src/App.js
@@ -3,71 +3,38 @@ import { googleLogout, useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
 
 function App() {
-  const styles = {
-    container: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      minHeight: "100vh",
-      backgroundColor: "#f5f5f5",
-    },
-    header: {
-      fontSize: "24px",
-      marginBottom: "20px",
-    },
-    content: {
-      backgroundColor: "#fff",
-      borderRadius: "8px",
-      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-      padding: "20px",
-      textAlign: "center",
-    },
-    profile: {
-      textAlign: "center",
-    },
-    profileImage: {
-      width: "100px",
-      height: "100px",
-      borderRadius: "50%",
-      marginBottom: "10px",
-    },
-    heading: {
-      fontSize: "18px",
-    },
-    paragraph: {
-      fontSize: "14px",
-      margin: "5px 0",
-    },
-    loginButton: {
-      backgroundColor: "#007bff",
-      color: "#fff",
-      border: "none",
-      borderRadius: "4px",
-      padding: "10px 20px",
-      fontSize: "14px",
-      cursor: "pointer",
-      transition: "background-color 0.2s ease",
-    },
-    logoutButton: {
-      backgroundColor: "#007bff",
-      color: "#fff",
-      border: "none",
-      borderRadius: "4px",
-      padding: "10px 20px",
-      fontSize: "14px",
-      cursor: "pointer",
-      transition: "background-color 0.2s ease",
-    },
-  };
-
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState(null);
   const [profile, setProfile] = useState([]);
+  const [emails, setEmails] = useState([]);
 
   const login = useGoogleLogin({
-    onSuccess: (codeResponse) => setUser(codeResponse),
+    onSuccess: (codeResponse) => {
+      console.log(codeResponse);
+      setUser(codeResponse);
+    },
     onError: (error) => console.log("Login Failed:", error),
   });
+
+  // Function to fetch emails
+  async function fetchEmails() {
+    // Set up Axios instance with base URL and headers
+    const axiosInstance = axios.create({
+      baseURL: "https://gmail.googleapis.com/gmail/v1/users/me/",
+      headers: {
+        Authorization: `Bearer ${user.access_token}`,
+      },
+    });
+    try {
+      const response = await axiosInstance.get("messages");
+      return response.data.messages;
+    } catch (error) {
+      console.error(
+        "Error fetching emails:",
+        error.response ? error.response.data : error.message
+      );
+      return [];
+    }
+  }
 
   useEffect(() => {
     if (user) {
@@ -83,6 +50,8 @@ function App() {
         )
         .then((res) => {
           setProfile(res.data);
+          console.log(res.data);
+          fetchEmails();
         })
         .catch((err) => console.log(err));
     }
@@ -95,25 +64,27 @@ function App() {
   };
 
   return (
-    <div style={styles.container}>
-      <h2 style={styles.header}>React Google Login</h2>
-      <div style={styles.content}>
-        {profile ? (
-          <div style={styles.profile}>
-            <img style={styles.profileImage} src={profile.picture} alt="user" />
-            <h3 style={styles.heading}>User Logged in</h3>
-            <p style={styles.paragraph}>Name: {profile.name}</p>
-            <p style={styles.paragraph}>Email Address: {profile.email}</p>
-            <button style={styles.logoutButton} onClick={logOut}>
-              Log out
-            </button>
+    <div>
+      <h2>React Google Login</h2>
+      <br />
+      <br />
+      {profile ? (
+        <div>
+          <img src={profile.picture} alt="user image" />
+          <h3>User Logged in</h3>
+          <p>Name: {profile.name}</p>
+          <p>Email Address: {profile.email}</p>
+          <br />
+          <br />
+          <button onClick={logOut}>Log out</button>
+
+          <div>
+            <h1>Your emails</h1>
           </div>
-        ) : (
-          <button style={styles.loginButton} onClick={login}>
-            Sign in with Google 🚀
-          </button>
-        )}
-      </div>
+        </div>
+      ) : (
+        <button onClick={() => login()}>Sign in with Google 🚀 </button>
+      )}
     </div>
   );
 }
